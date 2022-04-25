9324220516a7c923751e172bed02f20886455327
yvonne-nguyen <91553153+yvonne-nguyen@users.noreply.github.com>
2022-04-24 21:47:48 -0700

    Update map.js

--- week4/js/map.js
+++ week4/js/map.js
@@ -2,25 +2,26 @@
 let map;
 let lat = 37.3382;
 let lon = -121.8863;
-let zl = 10;
+let zl = 11;
 let path = "data/sjhousing.csv";
-
-// global variables
 let markers = L.featureGroup();
+
 
 // initialize
 $( document ).ready(function() {
     createMap(lat,lon,zl);
+	readCSV(path);
 });
 
 // create the map
 function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
+
 
 // function to read csv data
 function readCSV(path){
@@ -39,14 +40,30 @@ function readCSV(path){
 
 function mapCSV(data){
 	
-	// loop through each entry
-	data.data.forEach(function(item,index){
-		// create marker
-		let marker = L.marker([item.latitude,item.longitude])
+	// circle options
+	let circleOptions = {
+		radius: 6,
+		weight: 0.5,
+		color: 'white',
+		fillColor: '#ffffff',
+		fillOpacity: 0.4
+	}
 
-		// add marker to featuregroup
+
+	// loop through each entry
+	data.data.forEach(function(item, index){
+		// create marker
+		let marker = L.circleMarker([item.Latitude,item.Longitude],circleOptions)
+		.on('mouseover',function(){
+			this.bindPopup(`${item.ZipCode}<br>${item.AverageCost}</br>`).openPopup()
+		})
+
+		$('.sidebar').append(`<div class="sidebar-item" onmouseover="panTo(${index})"> ${item.ZipCode}</div>`)
+		
+		// add marker to featuregroup		
 		markers.addLayer(marker)
-	})
+})
+
 
 	// add featuregroup to map
 	markers.addTo(map)
@@ -54,3 +71,9 @@ function mapCSV(data){
 	// fit markers to map
 	map.fitBounds(markers.getBounds())
 }
+
+function panTo(index){
+	map.setZoom(14);
+	map.panTo(markers.getLayers()[index]._latlng);
+}
+
