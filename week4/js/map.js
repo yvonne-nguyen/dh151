fe660f03f368ff47608ee664d8d576adb8406b2a
yvonne-nguyen <91553153+yvonne-nguyen@users.noreply.github.com>
2022-04-24 18:18:49 -0700

    Create map.js

---
+++ week4/js/map.js
@@ -0,0 +1,68 @@
+// Global variables
+let map;
+let lat = 37.3382;
+let lon = -121.8863;
+let zl = 10;
+let path = "data/sjhousing.csv";
+
+// global variables
+let markers = L.featureGroup();
+
+// initialize
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+    readCSV(path);
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+function mapCSV(data){
+	
+	// circle options
+	let circleOptions = {
+		radius: 5,
+		weight: 1,
+		color: 'white',
+		fillColor: 'dodgerblue',
+		fillOpacity: 1
+	}
+
+	// loop through each entry
+        data.data.forEach(function(item,index){
+        if(typeof item.latitude !== 'undefined' && typeof item.longitude !== 'undefined'){
+            // create marker
+            let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+        }
+
+		// add marker to featuregroup		
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds())
+}
